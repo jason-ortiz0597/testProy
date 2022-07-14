1b68bc7b5079a6f6cae89124a3bf3fade0d9f690
siipi sistemas <eljros999xxx@gmail.com>
2022-07-14 16:47:01 -0400

    nuevo avanze

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ const subcategoriaRutas = require('./routes/ProductoRoutes/Subcategoria2');
 const claseRutas = require('./routes/ProductoRoutes/Clase2');
 const productoRutas = require('./routes/ProductoRoutes/Producto2');
 
+//use cors
+const cors = require('cors');
+
+
+
+
 
 
 // database
@@ -25,6 +31,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 //!------------------------------------------------------
 app.use('/api/category', categoryRoute);
